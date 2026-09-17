9961f0de363cedeb1cd3f12ed316772c319692cb
prateekdeshbhandari <prateekdeshbhandari@gmail.com>
2026-09-17 19:42:34 +0530

    Implement Spring Bean Factory with Setter Injection

--- Weather/src/main/java/com/xworkz/config/Weatherconfig.java
+++ Weather/src/main/java/com/xworkz/config/Weatherconfig.java
@@ -1,13 +1,77 @@
 package com.xworkz.config;
 
+import com.xworkz.component.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(" com.xworkz")
+@ComponentScan(" com.xworkz.component")
 public class Weatherconfig
 {
     public Weatherconfig(){
         System.out.println("create Weatherconfig");
+    }
+@Bean
+    public  Shield shield(){
+
+
+
+        Shield shielda = new Shield();
+        System.out.println("Shield created");
+return shielda;
+    }
+    @Bean
+    public String name(){
+        System.out.println("Name created");
+        return "Xworkz";
+    }
+
+    @Bean
+    public String location(){
+        System.out.println("Location created");
+        return "BTM";
+    }
+    @Bean
+    public  Long phone(){
+        System.out.println("Phone created");
+        return 1234567890L;
+    }
+
+    @Bean
+    public Toy toy(){
+        Toy toy=new Toy();
+        System.out.println("Toy created");
+        return toy;
+    }
+    @Bean
+    public Paint paint(){
+        Paint paint=new Paint();
+        System.out.println("Paint created");
+        return paint;
+    }
+    @Bean
+    public Camera camera(){
+        Camera camera=new Camera();
+        System.out.println("Camera created");
+        return camera;
+    }
+    @Bean
+    public String brand(){
+        System.out.println("Brand created");
+        return "Canon";
+
+    }
+    @Bean
+    public Ganesha ganesha(){
+        Ganesha ganesha=new Ganesha();
+        System.out.println("Ganesha created");
+        return  ganesha;
+    }
+    @Bean
+    public Elephan elephant(){
+        Elephan elephan=new Elephan();
+        System.out.println("Elephan created");
+        return elephan;
     }
 }
